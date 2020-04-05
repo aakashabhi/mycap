3f50cd1f0de7e19b5467ce745e43a1462362197f
aakashabhi <63002725+aakashabhi@users.noreply.github.com>
2020-04-05 12:13:19 +0530

    Create fibonacci.py
    Prints first n elements in the fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if(n==1):
+        return 0
+    elif(n==2):
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+print("Enter the Value of N")
+n=int(input())
+for i in range(1,n+1):
+    print(fibonacci(i))
